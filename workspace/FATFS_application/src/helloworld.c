e8b07d9dc592cd05e4dda22dcccfb68a328f8632
DavidJRichards <richards.malton@gmail.com>
2021-02-01 16:42:58 +0000

    More vitis projects

---
+++ workspace/FATFS_application/src/helloworld.c
@@ -0,0 +1,129 @@
+/******************************************************************************
+*
+* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+
+#include <string.h>
+#include "xparameters.h"
+#include "xil_printf.h"
+#include "ff.h"
+#include "xdevcfg.h"
+
+static FATFS fatfs;
+
+int SD_Init()
+{
+    FRESULT rc;
+    rc = f_mount(&fatfs,"",0);
+    if(rc)
+    {
+        xil_printf("ERROR : f_mount returned %d\r\n",rc);
+        return XST_FAILURE;
+    }
+    return XST_SUCCESS;
+}
+
+FRESULT scan_files (
+    char* path        /* Start node to be scanned (also used as work area) */
+)
+{
+    FRESULT res;
+    FILINFO fno;
+    DIR dir;
+    int i;
+    char *fn;   /* This function is assuming non-Unicode cfg. */
+#if _USE_LFN
+    static char lfn[_MAX_LFN + 1];
+    fno.lfname = lfn;
+    fno.lfsize = sizeof lfn;
+#endif
+
+
+    res = f_opendir(&dir, path);                       /* Open the directory */
+    if (res == FR_OK) {
+        i = strlen(path);
+        for (;;) {
+            res = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
+            if (fno.fname[0] == '.') continue;             /* Ignore dot entry */
+#if _USE_LFN
+            fn = *fno.lfname ? fno.lfname : fno.fname;
+#else
+            fn = fno.fname;
+#endif
+            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
+                sprintf(&path[i], "/%s", fn);
+                res = scan_files(path);
+                if (res != FR_OK) break;
+                path[i] = 0;
+            } else {                                       /* It is a file. */
+                xil_printf("%s/%s\n\r", path, fn);
+            }
+        }
+    }
+    else
+    {
+    	xil_printf("\n\ropendir error #%d\n\r",res);
+    }
+
+    return res;
+}
+
+int main()
+{
+    init_platform();
+    SD_Init();
+
+    print("\n\rFAT FS test\n\r");
+    scan_files("");
+
+    print("Successfully ran application");
+    cleanup_platform();
+    return 0;
+}
---
+++ workspace/FATFS_application_system/_ide/scripts/systemdebugger_fatfs_application_system_standalone.tcl
@@ -0,0 +1,29 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/FATFS_application_system/_ide/scripts/systemdebugger_fatfs_application_system_standalone.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/FATFS_application_system/_ide/scripts/systemdebugger_fatfs_application_system_standalone.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent JTAG-SMT2 210251A088A0" && level==0 && jtag_device_ctx=="jsn-JTAG-SMT2-210251A088A0-13722093-0"}
+fpga -file /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/FATFS_application/_ide/bitstream/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/ebaz4205_standalone/export/ebaz4205_standalone/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/FATFS_application/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow /home/david/Documents/GitHub/djrm-EBAZ4205/workspace/FATFS_application/Debug/FATFS_application.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A9*#0"}
+con
